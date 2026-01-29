cb906676e8fb0d68161d3928d4a3d123efdec39a
tejaskt <tejasktgeetanjali88@gmail.com>
2026-01-29 19:00:31 +0530

    DashboardView.kt fixed with no user.

--- app/src/main/java/com/example/roomdb/presentation/screen/dashboard/DashboardView.kt
+++ app/src/main/java/com/example/roomdb/presentation/screen/dashboard/DashboardView.kt
@@ -1,6 +1,7 @@
 package com.example.roomdb.presentation.screen.dashboard
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -43,7 +44,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.roomdb.R
@@ -144,22 +147,35 @@ fun DashboardView(
                 // cast user from UiState<List<User>> to List<User>
                 val users = (state as UiState.Success<List<User>>).data
 
-                LazyColumn(
-                    modifier = Modifier
-                        .padding(paddingValues)
-                        .fillMaxSize(),
+                if(users.isEmpty()){
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "There is no User...",
+                            style = MaterialTheme.typography.displaySmall
+                        )
+                    }
+                }else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(paddingValues)
+                            .fillMaxSize(),
 //                      .consumeWindowInsets(paddingValues),
 //                  contentPadding = paddingValues
-                    contentPadding = PaddingValues(ScreenSpace.Horizontal_Space)
-                ) {
-                    items(users) { user ->
-                        UserCard(
-                            onUserClick = onUserDetailsClick,
-                            user = user,
-                            onEditUserClick = onEditUserClick,
-                            onDeleteUserClick = {
-                                viewModel.deleteUser(user)
-                            })
+                        contentPadding = PaddingValues(ScreenSpace.Horizontal_Space)
+                    ) {
+                        items(users) { user ->
+                            UserCard(
+                                onUserClick = onUserDetailsClick,
+                                user = user,
+                                onEditUserClick = onEditUserClick,
+                                onDeleteUserClick = {
+                                    viewModel.deleteUser(user)
+                                })
+                        }
                     }
                 }
             }
@@ -177,7 +193,7 @@ fun UserCard(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable{onUserClick(user.id)}
+            .clickable { onUserClick(user.id) }
             .padding(vertical = 6.dp),
         shape = MaterialTheme.shapes.large,
         elevation = CardDefaults.cardElevation(2.dp),
